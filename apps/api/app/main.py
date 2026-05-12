b8397bd52a0b9b1d8a467ac555107383f5088396
Pjizen0143 <parabolic0143@gmail.com>
2026-05-13 00:05:52 +0700

    feat: Implement v1 users router

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
-from app.api.health import router as health_router
-from app.core.db import create_db_and_tables
+
 from app import models
+from app.api.health import router as health_router
+from app.api.v1.routers.user import router as user_router
+from app.core.config import settings
+from app.core.db import create_db_and_tables
 
 
 async def lifespan(app: FastAPI):
@@ -13,3 +16,4 @@ app = FastAPI(lifespan=lifespan)
 
 
 app.include_router(health_router)
+app.include_router(user_router, prefix=settings.API_V1_PREFIX)
