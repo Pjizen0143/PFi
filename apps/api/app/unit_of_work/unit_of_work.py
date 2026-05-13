e0d07123e6de7c48d56bab78f0045cab35cae853
Pjizen0143 <parabolic0143@gmail.com>
2026-05-13 19:12:09 +0700

    Create auth_service.py

--- apps/api/app/unit_of_work/unit_of_work.py
+++ apps/api/app/unit_of_work/unit_of_work.py
@@ -1,12 +1,14 @@
 from sqlmodel import Session
 
 from app.repositories.user_repository import UserRepository
+from app.repositories.auth_repository import AuthRepository
 
 
 class UnitOfWork:
     def __init__(self, session: Session) -> None:
         self.session = session
         self.users = UserRepository(session)
+        self.auth = AuthRepository(session)
 
     def commit(self) -> None:
         self.session.commit()
