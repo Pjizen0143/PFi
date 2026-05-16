2d04da6d6ff345a5321b5eeac503896fce261a4e
Pjizen0143 <parabolic0143@gmail.com>
2026-05-16 21:24:45 +0700

    test: setup dockerized integration testing

--- Makefile
+++ Makefile
@@ -3,3 +3,9 @@ dev:
 
 prod:
 	ENVIRONMENT=production docker compose up -d
+
+test:
+	@ENVIRONMENT=test docker compose -p pfi-test run --rm api-test; \
+	EXIT_CODE=$$?; \
+	ENVIRONMENT=test docker compose -p pfi-test down -v; \
+	exit $$EXIT_CODE
--- apps/api/app/tests/conftest.py
+++ apps/api/app/tests/conftest.py
@@ -1,8 +1,26 @@
+import os
+
 import pytest_asyncio
 
 from httpx import ASGITransport, AsyncClient
+from sqlmodel import SQLModel
 
 from app.main import app
+from app.core.db import engine
+
+# IMPORTANT:
+# Import all models before create_all
+from app.models.user import User
+from app.models.auth import AuthProvider
+
+
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def setup_database():
+    SQLModel.metadata.create_all(engine)
+
+    yield
+
+    SQLModel.metadata.drop_all(engine)
 
 
 @pytest_asyncio.fixture
