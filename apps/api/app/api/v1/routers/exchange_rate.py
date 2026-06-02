68c4c1ddef741aab42b0923e48a63bdf3e2a17b2
Pjizen0143 <parabolic0143@gmail.com>
2026-06-02 22:49:32 +0700

    refactor: Injectable redis

--- apps/api/app/api/v1/routers/exchange_rate.py
+++ apps/api/app/api/v1/routers/exchange_rate.py
@@ -3,10 +3,11 @@ from fastapi import APIRouter
 from app.schemas.v1.exchange_rate import ExchangeRateResponse
 from app.services.exchange_rate_service import ExchangeRateService
 from app.repositories.exchange_rate_provider import ExchangeRateProvider
+from app.core.redis import redis_client
 
 router = APIRouter(prefix="/exchange-rates", tags=["Exchange Rates"])
 
-service = ExchangeRateService(ExchangeRateProvider())
+service = ExchangeRateService(ExchangeRateProvider(), redis_client)
 
 
 @router.get(
