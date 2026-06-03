ffa646abf91f3633c73df3d7578d14877e3f17a7
Pjizen0143 <parabolic0143@gmail.com>
2026-06-04 01:42:28 +0700

    chore: off set state in effect rules

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -13,6 +13,12 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+    // Add custom rules here if needed.
+    "react-hooks/set-state-in-effect": "off"
+  },
+  }
 ]);
 
 export default eslintConfig;
