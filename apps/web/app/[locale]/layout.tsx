5e6bbc4f5f459aecaef2bb1d2d570b140e0e04cd
Pjizen0143 <parabolic0143@gmail.com>
2026-06-09 02:35:43 +0700

    Resolve merge conflict in locale layout

--- apps/web/app/[locale]/layout.tsx
+++ apps/web/app/[locale]/layout.tsx
@@ -2,13 +2,9 @@ import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { notFound } from "next/navigation";
 import { ThemeProvider } from "@/providers/theme-provider";
-<<<<<<< Updated upstream
-import "../globals.css"; 
-=======
 import SessionWatcher from "@/components/shared/session-watcher";
 import Providers from "@/providers/auth-provider";
 import "../globals.css";
->>>>>>> Stashed changes
 
 export default async function LocaleLayout({
   children,
@@ -28,13 +24,6 @@ export default async function LocaleLayout({
   return (
     <html lang={locale} translate="no" suppressHydrationWarning>
       <body suppressHydrationWarning>
-<<<<<<< Updated upstream
-        <ThemeProvider>
-          <NextIntlClientProvider messages={messages}>
-            {children}
-          </NextIntlClientProvider>
-        </ThemeProvider>
-=======
         <Providers>
           <ThemeProvider>
             <SessionWatcher>
@@ -42,7 +31,6 @@ export default async function LocaleLayout({
             </SessionWatcher>
           </ThemeProvider>
         </Providers>
->>>>>>> Stashed changes
       </body>
     </html>
   );
